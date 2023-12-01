ca0cf062d77dd98f161a37b757a4b46bb1a1ea75
Lukas Petr <lukas.petr.dev@gmail.com>
2023-12-01 13:27:58 +0100

    2023 day1, second part

--- 2023/1/run.py
+++ 2023/1/run.py
@@ -2,17 +2,41 @@
 import re
 import sys
 
-RE_PATTERN = re.compile(r"""^
-                            [^\d]*        # no digit
-                            (\d)          # capture digit
+DIGITS_MAP = {
+    "zero" : 0,
+    "one" : 1,
+    "two" : 2,
+    "three" : 3,
+    "four" : 4,
+    "five" : 5,
+    "six" : 6,
+    "seven" : 7,
+    "eight" : 8,
+    "nine" : 9,
+}
+
+DIGITS=list(DIGITS_MAP.keys());
+DIGITS.append(r"\d")
+# regex for digits
+D = '|'.join(DIGITS)
+
+RE_PATTERN = re.compile(rf"""^
+                             .*?           # non-greedy everything
+                            ({D})          # capture digit
                             (?:
-                                   .*     # anything
-                                   (\d)   # and capture digit
-                                   [^\d]* # followed by non-digit
-                                |         # or
-                                   [^\d]* # no more digits
+                                   .*      # anything
+                                   ({D})   # and capture digit
+                                   .*      # the rest
+                                |          # or
+                                   .*      # no more digits
                             )
-                            $""", re.VERBOSE | re.MULTILINE)
+                            $""", re.VERBOSE | re.MULTILINE | re.IGNORECASE)
+
+def get_digit(substr):
+    if substr in DIGITS_MAP:
+        return DIGITS_MAP[substr]
+    else:
+        return int(substr)
 
 file_path = sys.argv[1]
 
@@ -22,11 +46,11 @@ with open(file_path, "r") as file:
     for line in file:
         line = line.strip()
         result = RE_PATTERN.search(line)
+        # when using print gives broken pipe error, why??
         if  result.group(2) is None:
-            number = int(result.group(1)) * 10 + int(result.group(1))
+            number = get_digit(result.group(1)) * 10 + get_digit(result.group(1))
         else:
-            number = int(result.group(1)) * 10 + int(result.group(2))
-        print(line, number)
+            number = get_digit(result.group(1)) * 10 + get_digit(result.group(2))
         # todo other posibilities
         sum += number
 print(sum)
